3d0e4b2b107ced82060401db99bcb25bea722aee
PierreAlexisRoy <pierre-alexis_roy@hotmail.ca>
2020-01-28 12:55:19 -0500

    jan28 morning data

--- Lab1_Zeeman/error_intensity.py
+++ Lab1_Zeeman/error_intensity.py
@@ -3,11 +3,19 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 
-im = Image.open('IMG_4399.JPG').convert('F')
+im = Image.open('FP_pattern_91.6mV_j0.JPG').convert('F')
 pixels_1 = np.array(im)
 
-im = Image.open('IMG_4400.JPG').convert('F')
+plt.imshow(pixels_1)
+plt.colorbar()
+plt.show()
+
+im = Image.open('FP_pattern_91.6mV_j1.JPG').convert('F')
 pixels_2 = np.array(im)
+
+plt.imshow(pixels_2)
+plt.colorbar()
+plt.show()
 
 print(np.mean(np.absolute(pixels_1-pixels_2)))
 
