4af3d503a97e1158f371f474155a8f42694d1e34
PierreAlexisRoy <pierre-alexis_roy@hotmail.ca>
2020-02-26 17:28:06 -0500

    fit mica

--- Lab2_Mossbauer/plot_YFeO3_18.py
+++ Lab2_Mossbauer/plot_YFeO3_18.py
@@ -7,7 +7,7 @@ import matplotlib.gridspec as gridspec
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
 
-data = np.loadtxt('True_folded_data_micaplatestraight0214.txt', delimiter=',')
+data = np.loadtxt('True_folded_data_YFeO3_room_temperature18C.txt', delimiter=',')
 
 x = fit(data[:,0][:-1])
 y = data[:,1][:-1]
@@ -20,21 +20,21 @@ err_x = np.sqrt((6e-5*x)**2 +0.009**2)
 # exit(1)
 
 # flat line background level
-flat = np.max(y)
+flat = np.mean(y[:10])
 
-p0=[0.1, flat, 14.39999999997, 28.80000000011, 0.5, 10000]
+p0=[0.2, flat, 14.39999999997, 0.18088/1.752, 33, 28.80000000011, 80000, 70000, 35000]
 
-func = MossbauerModel(Zeeman=False, quad=False, quad_angle=True).mod
+func = MossbauerModel(Zeeman=True, quad=True, quad_angle=False).mod
 
 plt.plot(x,y, '.')
 x_test = np.linspace(-10,10,10000000)
-y_test = func(x_test, p0[0], p0[1], p0[2], p0[3], p0[4], p0[5])
+y_test = func(x_test, p0[0], p0[1], p0[2], p0[3], p0[4], p0[5], p0[6], p0[7], p0[8])
 plt.plot(x_test,y_test)
 plt.show()
 
-#assert(1==0)
+assert(1==0)
 
-p,pcov = curve_fit(func,x,y, p0=p0, bounds=([0, flat-5000, 14.39999999996, 28.800000000105, -np.pi/10, 0],[0.3, flat+5000, 14.39999999998, 28.80000000012, np.pi/4, 30000]), sigma = yerr)
+p,pcov = curve_fit(func,x,y, p0=p0, bounds=([0, flat-5000, 14.39999999996, 28.800000000105, 0, 0],[0.3, flat+5000, 14.39999999998, 28.80000000012, np.pi, 50000]), sigma = yerr)
 for i in range(len(p)):
 	print(p[i], np.sqrt(pcov[i,i]))
 x_arr = np.linspace(x[0],x[-1],10000000)
@@ -79,5 +79,5 @@ ax2.yaxis.set_minor_locator(AutoMinorLocator())
 ax2.set_xlabel('Velocity (mm/s)', size = 24)
 ax2.set_ylabel('Data-Fit (Counts)', size = 24)
 ax2.legend( prop={'size': 20})
-# plt.show()
-plt.savefig('Fit_micaPowder_Straight.png',bbox_inches='tight')
+plt.show()
+#plt.savefig('Fit_YfeO3_18.png',bbox_inches='tight')
--- Lab2_Mossbauer/plot_micaPowder.py
+++ Lab2_Mossbauer/plot_micaPowder.py
@@ -7,10 +7,10 @@ import matplotlib.gridspec as gridspec
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
 
-data = np.loadtxt('folded_data_mica0212.txt', delimiter=',')
+data = np.loadtxt('True_folded_data_mica0212.txt', delimiter=',')
 
-x = fit(data[:,0][1:])
-y = np.flip(data[:,1][1:])
+x = fit(data[:,0][:-1])
+y = data[:,1][:-1]
 yerr = np.sqrt(y)
 err_x = np.sqrt((6e-5*x)**2 +0.009**2)
 
@@ -22,14 +22,22 @@ err_x = np.sqrt((6e-5*x)**2 +0.009**2)
 # flat line background level
 flat = np.max(y)
 
-p0=[0.15, flat, 30000, -100000, 0, 12500]
+p0=[0.1, flat, 14.39999999997, 28.80000000011, 0.5, 10000]
 
 func = MossbauerModel(Zeeman=False, quad=False, quad_angle=True).mod
 
-p,pcov = curve_fit(func,x,y, p0=p0, bounds=([0, flat-5000,1000,-200000, -np.pi/4, 0],[0.4, flat+5000, 35000, 200000, np.pi/2, 15000]), sigma = yerr)
+plt.plot(x,y, '.')
+x_test = np.linspace(-10,10,10000000)
+y_test = func(x_test, p0[0], p0[1], p0[2], p0[3], p0[4], p0[5])
+plt.plot(x_test,y_test)
+plt.show()
+
+#assert(1==0)
+
+p,pcov = curve_fit(func,x,y, p0=p0, bounds=([0, flat-5000, 14.39999999996, 28.800000000105, -np.pi/4, 0],[0.4, flat+5000, 14.39999999998, 28.80000000012, np.pi/2, 15000]), sigma = yerr)
 for i in range(len(p)):
 	print(p[i], np.sqrt(pcov[i,i]))
-x_arr = np.linspace(-10,10,10000000)
+x_arr = np.linspace(x[0],x[-1],10000000)
 
 # plt.plot(x_arr,func(x_arr,0.1,np.max(y), 1.44e+04,(0.18088)/(-1.752), 33000, 1e2, 50, 20))
 # plt.plot(x_arr,func(x_arr,p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7]))
